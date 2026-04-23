a3754f7fa65b5a71a4500ec0fc1d9a05b0fdb265
junaietfaruk <junaietfaruk@throughltd.com>
2026-04-23 07:37:41 +0600

    navbar develop

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Navbar from './shared/Navbar'
+
+function App() {
+  return (
+   <>
+      <div className='w-480 mx-auto'>
+
+        <Navbar />
+</div>
+   
+   </>
+  )
+}
+
+export default App
---
+++ src/shared/Heading.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Heading = () => {
+    return (
+        <div className="bg-[#183497] h-[42px] flex items-center justify-center">
+            <p className="text-white text-[16px] font-normal font-[Manrope] leading-[24px] text-center ">
+                Autodesigner 2.0 is here. The most popular UI generator just got even better!
+            </p>
+        </div>
+    );
+};
+
+export default Heading;
